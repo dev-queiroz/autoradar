b104fe4203460d3c8e2ace34f3efc05bd3246229
dev-queiroz <douglasqueiroz@alu.ufc.br>
2026-08-05 10:31:07 -0300

    feat(components): add reausable Container layout component

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "AutoRadar",
+  description:
+    "Discover, compare and find where to buy vehicles.",
+  url: "https://autoradar.vercel.app",
+  github: "https://github.com/devqueiroz/autoradar",
+} as const;
